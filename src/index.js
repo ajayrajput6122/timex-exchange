a2def93fdfff838743c5f5b5dc36b385ca693c06
ajayrajput6122 <ajay.jstech@gmail.com>
2024-11-19 15:04:50 +0530

    all code

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-        <Toaster position="top center" />
+        <Toaster containerClassName="popup_main" position="top center" />
         <App />
     </AuthProvider>
   </React.StrictMode>
