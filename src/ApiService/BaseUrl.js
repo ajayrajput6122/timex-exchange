4f89f9dc17d7b3e47dbbf3365bd6be20a8b2b46e
RamanJsTech <jstehraman@gmail.com>
2024-12-16 10:37:30 +0530

    ..

--- src/ApiService/BaseUrl.js
+++ src/ApiService/BaseUrl.js
@@ -1,2 +1,2 @@
-// export const base_url = `http://192.168.1.8:8080`;
-export const base_url = `https://api.Tomax.exchange`;   
+// export const base_url = `http://192.168.1.72:8080`;
+export const base_url = `https://api.Tomax.exchange`;     
